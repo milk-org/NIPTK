69872688b945b984f085caee84ecb1bc7403b98f
oguyon <oliv.guyon@gmail.com>
2018-02-01 10:20:00 -1000

    compile

--- src/CLImain.c
+++ src/CLImain.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <CommandLineInterface/CLIcore.h>
 
+
+
+#include <image_basic/image_basic.h>
+#include <image_format/image_format.h>
+#include <img_reduce/img_reduce.h>
+#include <psf/psf.h>
+#include <ZernikePolyn/ZernikePolyn.h>
+
+
+
 #define STYLE_BOLD    "\033[1m"
 #define STYLE_NO_BOLD "\033[22m"
 
@@ -13,6 +23,15 @@ int main(int argc, char *argv[])
 	printf(STYLE_BOLD);
 	printf("\n        Multi-purpose Imaging Libraries toolKit (milk)\n");
 	printf(STYLE_NO_BOLD);
+	
+	
+	
+	// initialize milk modules for which no function calls is included by default
+	libinit_image_basic();
+	libinit_image_format();
+	libinit_psf();
+	libinit_img_reduce();
+	libinit_ZernikePolyn();
 
 	runCLI(argc, argv, AppName);
 
